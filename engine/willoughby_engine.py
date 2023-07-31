02a2ec9440eda4af389fa7dcffba41c2d5a3b813
lblommesteyn <lblommes@uwo.ca>
2023-07-31 10:53:21 -0400

    New Design Implemented

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -9,5 +9,6 @@ class WilloughbyEngine(Car, ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
+        # Service criteria for WilloughbyEngine: Once every 60,000 miles
         return self.current_mileage - self.last_service_mileage > 60000
